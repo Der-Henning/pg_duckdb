24bb0663e63fcc67797d7d481635fcb34ece5ade
Henning Merklinger <henning.merklinger@gmail.com>
2025-02-22 10:46:12 +0000

    test

--- src/pgduckdb_options.cpp
+++ src/pgduckdb_options.cpp
@@ -182,7 +182,8 @@ ReadDuckdbSecrets() {
 			secret.connection_string = DatumToString(datum_array[Anum_duckdb_secret_connection_string - 1]);
 
 		if (!is_null_array[Anum_duckdb_secret_url_style - 1])
-			secret.url_style = StringToUrlStyle(DatumToString(datum_array[Anum_duckdb_secret_url_style - 1]));
+			auto path_str = DatumToString(datum_array[Anum_duckdb_secret_url_style - 1]);
+			secret.url_style = StringToUrlStyle(path_str);
 		else
 			secret.url_style = UrlStyle::UNDEFINED;
 
